9c9baf4a2bcf37b8757c91a24a287eb5b6c2c7af
tejaswi551 <tejaswipyla2007@gmail.com>
2025-09-06 15:01:48 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,68 @@
+let x=2,y=2;
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)
+console.log(x**y)
+
+let z=2;
+z+=3;
+console.log(z)
+z-=5;
+console.log(z)
+z/=2;
+console.log(z)
+z*=5;
+console.log(z)
+
+//comparision operators
+let a=10,b="10";
+console.log(a==b)
+console.log(a===b)
+console.log(a!==b)
+console.log(a>=b)
+console.log(a<=b)
+
+//logical operators
+let age=18;
+console.log(age>=18&&age<=18)
+console.log(age>18||age<18)
+console.log(!(age<=18))
+
+//string operators
+
+let firstname="pyla";
+let lastname="tejaswi";
+let fullname=firstname+" "+lastname;
+console.log(fullname)
+let correctname=`hello ${firstname} ${lastname},welcome`;
+console.log(correctname)
+
+//ternary operator
+
+let myage=18;
+if(myage>=18){
+    console.log("eligible to vote")
+}else{
+    console.log("u can't vote")
+}
+
+let count=2;
+console.log(count++)
+console.log(count)
+console.log(++count)
+
+console.log(typeof count)
+console.log(typeof "tej")
+
+
+
+
+
+
+
+
+
+
+
--- sample.js
+++ sample.js
@@ -1,1 +1,31 @@
 console.log("this is pyla")
+let name="dev astra";
+console.log(name)
+
+let no=18;
+console.log(no)
+
+let islogged=false;
+console.log(islogged)
+ 
+let x;
+console.log(x)
+console.log(typeof name)
+console.log(typeof no)
+console.log(typeof islogged)
+console.log(typeof x)
+
+let arr=[2,4,6,8]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person={
+    name:"teja",
+    age:18
+}
+console.log(person[
+    'name'
+])
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)

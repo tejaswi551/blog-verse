14ad75dcb86c5d371b49883b8b7d84f8d53b2ab0
tejaswi551 <tejaswipyla2007@gmail.com>
2025-09-06 15:54:36 +0530

    function

---
+++ functions.js
@@ -0,0 +1,24 @@
+function greet(){
+    console.log("welcome")
+}
+greet()
+
+function sub(x,y){
+    return x-y
+}
+let result=sub(7,3);
+console.log(result)
+  
+
+const pi=3.14
+console.log(pi)
+
+
+const square=(x)=>x*x;
+console.log(square(5))
+
+const add =(x,y)=>{
+    console.log(x,y)
+    return x+y;
+}
+console.log(add(10,20))
